e6987e6c2653bcbe9c90b6f11117b58848865a35
aleramirezsj <aleramirezsj@gmail.com>
2021-06-15 15:44:13 -0300

    Inicio del proyecto

--- Presentacion/Form1.Designer.cs
+++ Presentacion/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Presentacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(836, 450);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
