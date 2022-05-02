072a65e56bf2e74bff07396ddd085b2cf631d0b8
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2022-05-02 13:54:04 -0300

    probando left join en linq

--- PruebasPorConsola/Program.cs
+++ PruebasPorConsola/Program.cs
@@ -6,7 +6,37 @@ namespace PruebasPorConsola
     {
         static void Main(string[] args)
         {
-            MenuPantalla();
+            var opcion = 0;
+            do
+            {
+                MenuPantalla();
+                opcion=int.Parse(Console.ReadLine());
+                LlamarAEjercicio(opcion);
+                Console.ReadKey();
+            } while (opcion!=9);
+        }
+
+        private static void LlamarAEjercicio(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    new Ejercicio1();
+                    break;
+                case 2:
+                    new Ejercicio2();
+                    break;
+                case 3:
+                    new Ejercicio3();
+                    break;
+                case 4:
+                    new Ejercicio4();
+                    break;
+                default:
+                    Console.WriteLine("Se ingresó un valor no válido(presione cualquier tecla para continuar)");
+                    Console.ReadKey();
+                    break;
+            }
         }
 
         static void MenuPantalla()
@@ -17,6 +47,7 @@ namespace PruebasPorConsola
             Console.WriteLine("2-Ejercicio Loop de impares:");
             Console.WriteLine("3-Ejercicio Sumattions:");
             Console.WriteLine("4-Ejercicio Callbacks:");
+            Console.WriteLine("9-Para salir");
             Console.Write("Ingrese una opción");
         }
 
